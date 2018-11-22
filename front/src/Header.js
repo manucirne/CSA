07fafa9761b69f6af258be2c8dcb07a3cd75fa62
IagoMendes <iagorm@al.insper.edu.br>
2018-11-22 11:47:49 -0200

    header com router quase pronto

--- front/src/Header.js
+++ front/src/Header.js
@@ -7,9 +7,8 @@ import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import AssessmentIcon from '@material-ui/icons/Assessment';
 import FoodIcon from '@material-ui/icons/LocalDining';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './Header.css';
-import CrudContainer from './CrudContainer'
-
 
 class Header extends Component {
     constructor(props){
@@ -27,32 +26,67 @@ class Header extends Component {
 
     render(){
         return(
-        <div className="root">
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton className="menuButton" color="inherit" aria-label="Menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" className="grow">
-              CSA
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-        
-        <CrudContainer></CrudContainer>
-        
-        <BottomNavigation
-          value={this.state.value}
-          onChange={this.handleChange}
-          showLabels
-        >
-        <BottomNavigationAction label="Informações" icon={<AssessmentIcon />} />
-        <BottomNavigationAction label="Receitas" icon={<FoodIcon />} />
-      </BottomNavigation>
-      </div>
+          <Router>
+            <div className="root">
+              <AppBar position="static">
+                <Toolbar>
+
+                  <IconButton className="menuButton" color="inherit" aria-label="Menu">
+                    <MenuIcon />
+                  </IconButton>
+
+                    <Typography variant="h6" color="inherit" className="grow">
+                        CSA
+                    </Typography>
+                    
+                      <Button color="inherit">
+                        <Link to="/login" style={{textDecoration:'none', color:'white'}}>Login</Link>
+                      </Button>
+                  
+                </Toolbar>
+              </AppBar>
+
+              <BottomNavigation value={this.state.value} onChange={this.handleChange} showLabels>
+                  <Link to="/" style={{textDecoration:'none'}}>
+                    <BottomNavigationAction label="Informações" icon={<AssessmentIcon/>} showLabel={true}/>
+                  </Link>
+                  
+                  <Link to="/receitas" style={{textDecoration:'none'}}> 
+                    <BottomNavigationAction label="Receitas" icon={<FoodIcon/>} showLabel={true}/>
+                  </Link>
+              </BottomNavigation>
+
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/receitas" component={Receitas} />
+            </div>
+          </Router>  
         );
     }
 }
 
+function Home() {
+  return (
+    <div>
+      <h2>FOI</h2>
+    </div>
+  );
+}
+
+function Login(props) {
+
+  return (
+    <div>
+      <h2>AQUI VAI PRO LOGIN</h2>
+    </div>
+  );
+}
+
+function Receitas() {
+  return (
+    <div>
+      <h2>OLHA O BOLOOOO</h2>
+    </div>
+  );
+}
 export default Header;
