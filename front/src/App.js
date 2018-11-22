9d3d9ab925ae5b7a24951fe594d71dd73af4cf46
Bruno Costa <brunex887@gmail.com>
2018-11-22 09:21:11 -0200

    Template de sidebar feito e botão funcionando

--- front/src/App.js
+++ front/src/App.js
@@ -7,26 +7,71 @@ import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import AssessmentIcon from '@material-ui/icons/Assessment';
 import FoodIcon from '@material-ui/icons/LocalDining';
+import Drawer from '@material-ui/core/Drawer';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const styles = {
+  list: {
+    width: 250,
+  },
+};
 
 class App extends Component {
   state = {
-    value: 0
+    value: 0,
+    left: false,
   };
 
   handleChange = (event, value) => {
     this.setState({ value });
   };
 
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open,
+    });
+  };
+
   render() {
 
-    const { classes } = this.props;
     const { value } = this.state;
+    const { classes } = this.props;
+
+    const sideList = (
+      <div className= "sidebar">
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
 
     return (
       <div className="App-header">
         <AppBar position="static">
           <Toolbar>
-            <IconButton color="inherit" aria-label="Menu">
+            <IconButton onClick={this.toggleDrawer('left', true)} color="inherit" aria-label="Menu">
               <MenuIcon />
             </IconButton>
             <Typography variant="h6" color="inherit">
@@ -35,6 +80,16 @@ class App extends Component {
             <Button color="inherit">Login</Button>
           </Toolbar>
         </AppBar>
+        <Drawer open={this.state.left} onClose={this.toggleDrawer('left', false)}>
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.toggleDrawer('left', false)}
+            onKeyDown={this.toggleDrawer('left', false)}
+          >
+            {sideList}
+          </div>
+        </Drawer>
 
         <BottomNavigation
           value={value}
