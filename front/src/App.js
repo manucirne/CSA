5d62e2021af766060e040bf028cfba8d31cab891
vitorsv1 <vitorsv1@al.insper.edu.br>
2018-11-22 08:40:31 -0200

    Ta bunitu

--- front/src/App.js
+++ front/src/App.js
@@ -1,51 +1,12 @@
 import React, { Component } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import { Toolbar, IconButton, Typography } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
-import MenuIcon from '@material-ui/icons/Menu';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import AssessmentIcon from '@material-ui/icons/Assessment';
-import FoodIcon from '@material-ui/icons/LocalDining';
 
 class App extends Component {
-  state = {
-    value: 0
-  };
-
-  handleChange = (event, value) => {
-    this.setState({ value });
-  };
 
   render() {
-
-    const { classes } = this.props;
-    const { value } = this.state;
-
     return (
-      <div className="App-header">
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton color="inherit" aria-label="Menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit">
-              CSA
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-
-        <BottomNavigation
-          value={value}
-          onChange={this.handleChange}
-          showLabels
-        >
-        <BottomNavigationAction label="Informações" icon={<AssessmentIcon />} />
-        <BottomNavigationAction label="Receitas" icon={<FoodIcon />} />
-      </BottomNavigation>
+      <div className="App">
+        <a href="/home">Home</a>
       </div>
-      
     );
   }
 }
--- front/src/Header.js
+++ front/src/Header.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
-
+import AppBar from '@material-ui/core/AppBar';
+import { Toolbar, IconButton, Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import AssessmentIcon from '@material-ui/icons/Assessment';
+import FoodIcon from '@material-ui/icons/LocalDining';
+import './Header.css';
 
 class Header extends Component {
+    constructor(props){
+        super(props)
+        console.log(props)
+    }
+
+    state = {
+        value: 0
+    };
+
+    handleChange = (event, value) => {
+    this.setState({ value });
+    };
 
     render(){
         return(
-            Header
+        <div className="root">
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className="menuButton" color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className="grow">
+              CSA
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+
+        <BottomNavigation
+          value={this.state.value}
+          onChange={this.handleChange}
+          showLabels
+        >
+        <BottomNavigationAction label="Informações" icon={<AssessmentIcon />} />
+        <BottomNavigationAction label="Receitas" icon={<FoodIcon />} />
+      </BottomNavigation>
+      </div>
         );
     }
 }
