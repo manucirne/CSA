67067c6426ede5dda599590eb09daa6f40dec3dd
vitorsv1 <vitorsv1@al.insper.edu.br>
2018-11-22 09:38:10 -0200

    App main

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import { Toolbar, IconButton, Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import AssessmentIcon from '@material-ui/icons/Assessment';
+import FoodIcon from '@material-ui/icons/LocalDining';
+import './App.css';
 
 class App extends Component {
 
+  constructor(props){
+    super(props)
+    console.log(props)
+  }
+
+  state = {
+      value: 0
+  };
+
+  handleChange = (event, value) => {
+  this.setState({ value });
+  };
+
+
   render() {
     return (
-      <div className="App">
-        <a href="/home">Home</a>
+      <div className="root">
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className="menuButton" color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className="grow">
+              CSA
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+
+        <BottomNavigation
+          value={this.state.value}
+          onChange={this.handleChange}
+          showLabels
+        >
+        <BottomNavigationAction label="Informações" icon={<AssessmentIcon />} />
+        <BottomNavigationAction label="Receitas" icon={<FoodIcon />} />
+      </BottomNavigation>
       </div>
     );
   }
