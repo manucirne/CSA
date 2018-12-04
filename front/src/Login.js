0256fe0247aab1a08f4c38de4a682b71870b07a4
LiuSeeker <vitor.liu@gmail.com>
2018-12-04 19:15:03 -0200

    login feito

--- front/src/Login.js
+++ front/src/Login.js
@@ -6,6 +6,41 @@ import { Link } from "react-router-dom";
 
 class Login extends Component {
 
+  constructor(props){
+    super(props)
+    this.state={
+      username: "",
+      passsword: "",
+      _id: "",
+      response: null
+    }
+  }
+
+  login = async () => {
+    try {
+      let response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: this.state.username,
+          password: this.state.password})
+      })
+    
+      let responseJson = await response.json()
+      this.state._id = responseJson._id;
+      console.log("fhsdgdfg");
+      console.log(this.state);
+
+    } catch (error) {
+      console.log(error)
+    }    
+  }
+
+
+
   render() {
     return (
       <div className="root">
@@ -16,15 +51,19 @@ class Login extends Component {
 
         <form>
           <div class="group">
-            <input type="text"/><span class="highlight"></span><span class="bar"></span>
+            <input id="login" type="text"    onChange={(ev) => {
+              this.setState({ username: ev.target.value })
+            }}/><span class="highlight"></span><span class="bar"></span>
+
             <label>Usuário</label>
           </div>
           <div class="group">
-            <input type="email"/><span class="highlight"></span><span class="bar"></span>
+            <input id="senha" type="email" onChange={(ev) => {
+              this.setState({ password: ev.target.value })}} /><span class="highlight"></span><span class="bar"></span>
             <label>Senha</label>
           </div>
           <Link to="/">
-            <Button type="button" class="button buttonBlue" onClick={this.props.stateFunction} >
+            <Button type="button" class="button buttonBlue" onClick={this.login} >
               Confirmar
               <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
             </Button>
