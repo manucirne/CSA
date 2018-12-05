c5eb5c6257d6bba57ad38d95615569cdf3e71cea
Bruno Costa <brunex887@gmail.com>
2018-12-05 13:04:17 -0200

    Receitas done

--- front/src/Card.js
+++ front/src/Card.js
@@ -12,13 +12,16 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import red from '@material-ui/core/colors/red';
 import EditIcon from '@material-ui/icons/Edit';
-import Logo from './img/bolo-de-cenoura.jpeg'
+import Logo from './img/bolo-de-cenoura.jpeg';
+import Torta from './img/torta-de-abobrinha.jpg';
+import Frango from './img/strogonoff-de-frango.jpg';
 import Collapse from '@material-ui/core/Collapse';
 import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
+import './Card.css';
 
 
 const styles = theme => ({
@@ -114,8 +117,8 @@ class RecipeCard extends React.Component {
             </div>
           </form>
         </div>
-        <div id="Card">
-          <Card className={classes.card}>
+        <div id="Card0" className="Card">
+          <Card className={classes.card} margin='normal'>
             <CardHeader
               avatar={
                 <Avatar aria-label="Recipe" className={classes.avatar}>
@@ -133,6 +136,156 @@ class RecipeCard extends React.Component {
             <CardMedia
               className={classes.media}
               image={Logo}
+              title="Paella dish"
+            />
+            <CardContent>
+              <Typography component="p">
+              <ul>
+                <li>3 cenouras médias raspadas e picadas</li>
+                <li>3 ovos</li>
+                <li>1 xícara de óleo</li>        
+              </ul>
+              Depois unte e enfarinhe uma forma e despeje a massa nela. Asse em forno médio por cerca de 40 minutos. Tire do forno, espere amornar e desenforme.
+              </Typography>
+              <CardActions className={classes.actions} disableActionSpacing>
+                <IconButton title="Edit" aria-label="Edit">
+                  <EditIcon />
+                </IconButton>
+                <IconButton
+                  className={classnames(classes.expand, {
+                    [classes.expandOpen]: this.state.expanded,
+                  })}
+                  onClick={this.handleExpandClick}
+                  aria-expanded={this.state.expanded}
+                  aria-label="Show more"
+                > 
+                <ExpandMoreIcon />
+                </IconButton>
+              </CardActions>
+            </CardContent>
+            <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>Method:</Typography>
+                <Typography paragraph>
+                  Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                  minutes.
+                </Typography>
+                <Typography paragraph>
+                  Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+                  heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+                  browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
+                  chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
+                  salt and pepper, and cook, stirring often until thickened and fragrant, about 10
+                  minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                </Typography>
+                <Typography paragraph>
+                  Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+                  without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat
+                  to medium-low, add reserved shrimp and mussels, tucking them down into the rice, and
+                  cook again without stirring, until mussels have opened and rice is just tender, 5 to 7
+                  minutes more. (Discard any mussels that don’t open.)
+                </Typography>
+                <Typography>
+                  Set aside off of the heat to let rest for 10 minutes, and then serve.
+                </Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
+        </div>
+        <div id="Card1" className="Card">
+          <Card className={classes.card} margin='normal'>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="Recipe" className={classes.avatar}>
+                  R
+                </Avatar>
+              }
+              action={
+                <IconButton title="Delete" aria-label="Delete">
+                  <DeleteRoundedIcon />
+                </IconButton>
+              }
+              title="Usuário"
+              subheader="Data DD/MM/AAAA"
+            />
+            <CardMedia
+              className={classes.media}
+              image={Frango}
+              title="Paella dish"
+            />
+            <CardContent>
+              <Typography component="p">
+              <ul>
+                <li>3 cenouras médias raspadas e picadas</li>
+                <li>3 ovos</li>
+                <li>1 xícara de óleo</li>        
+              </ul>
+              Depois unte e enfarinhe uma forma e despeje a massa nela. Asse em forno médio por cerca de 40 minutos. Tire do forno, espere amornar e desenforme.
+              </Typography>
+              <CardActions className={classes.actions} disableActionSpacing>
+                <IconButton title="Edit" aria-label="Edit">
+                  <EditIcon />
+                </IconButton>
+                <IconButton
+                  className={classnames(classes.expand, {
+                    [classes.expandOpen]: this.state.expanded,
+                  })}
+                  onClick={this.handleExpandClick}
+                  aria-expanded={this.state.expanded}
+                  aria-label="Show more"
+                > 
+                <ExpandMoreIcon />
+                </IconButton>
+              </CardActions>
+            </CardContent>
+            <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>Method:</Typography>
+                <Typography paragraph>
+                  Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                  minutes.
+                </Typography>
+                <Typography paragraph>
+                  Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+                  heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+                  browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
+                  chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
+                  salt and pepper, and cook, stirring often until thickened and fragrant, about 10
+                  minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                </Typography>
+                <Typography paragraph>
+                  Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+                  without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat
+                  to medium-low, add reserved shrimp and mussels, tucking them down into the rice, and
+                  cook again without stirring, until mussels have opened and rice is just tender, 5 to 7
+                  minutes more. (Discard any mussels that don’t open.)
+                </Typography>
+                <Typography>
+                  Set aside off of the heat to let rest for 10 minutes, and then serve.
+                </Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
+        </div>
+        <div id="Card2" className="Card">
+          <Card className={classes.card} margin='normal'>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="Recipe" className={classes.avatar}>
+                  R
+                </Avatar>
+              }
+              action={
+                <IconButton title="Delete" aria-label="Delete">
+                  <DeleteRoundedIcon />
+                </IconButton>
+              }
+              title="Usuário"
+              subheader="Data DD/MM/AAAA"
+            />
+            <CardMedia
+              className={classes.media}
+              image={Torta}
               title="Paella dish"
             />
             <CardContent>
